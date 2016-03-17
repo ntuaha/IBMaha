bcc570703d617f1f43bc471346b0193a20a8d460
aha <ntuaha@gmail.com>
2016-03-18 00:51:49 +0800

    增加測試資料

--- routes/esb/iQA.js
+++ routes/esb/iQA.js
@@ -1,8 +1,8 @@
 const net = require('net');
+const iconv = require('iconv-lite');
 const ROBOT_IP = "220.135.150.233";
 const ROBOT_PORT = 3388;
 
-var recv = "";
 
 function getDateString(d){
   var year = d.getFullYear();
@@ -19,35 +19,46 @@ function getDateString(d){
   return year+"/"+month+"/"+day+" "+hour+":"+min+":"+sec;
 }
 
+function fromBufToUTF8(buf){
+  return iconv.decode(buf, "big5");
+}
+function encodeBig(string){
+  return iconv.decode(iconv.encode(string,"utf8"),"utf8");
+}
 
-function sendMessage(ip,port,server_no,user_no,msg){
+
+function sendMessage(ip,port,server_no,user_no,msg,callback){
   var client = new net.Socket();
   client.connect(port, ip, function() {
-    console.log('Connected');
+    //console.log('Connected');
     var a = [];
-    a.push(getDateString(new Date()));
+    a.push(getDateString(new Date()).split(" ")[1]);
     a.push(user_no);
     a.push(server_no);
     a.push("CQ");
     a.push(msg);
-    client.write(a.join(","));
+    var send = iconv.encode(a.join(","),'big5');
+    //console.log("send: "+ send);
+    client.write(send);
   });
 
-  client.on('data', function(data) {
-    console.log('Received: ' + data);
-    recv = data;
+  client.on('data', function(buf) {
+    var recv = fromBufToUTF8(buf).split(",")[3];
+    //console.log("recv: "+recv);
+    callback(recv);
     client.destroy(); // kill client after server's response
+
   });
 
   client.on('close', function() {
-    console.log('Connection closed 2');
+    //console.log('Connection closed 2');
   });
 }
 
-function getMessageServer(msg){
+function getMessageServer(msg,callback){
   var client = new net.Socket();
   client.connect(ROBOT_PORT, ROBOT_IP, function() {
-    console.log('Connected');
+    //console.log('Connected');
     var a = [];
     a.push(getDateString(new Date()));
     a.push("1");
@@ -58,24 +69,23 @@ function getMessageServer(msg){
     client.write(a.join(","));
   });
 
-  client.on('data', function(data) {
-    var cols = data.split(",");
+  client.on('data', function(buf) {
+    //console.log(fromBufToUTF8(buf));
+    var cols = fromBufToUTF8(buf).split(",");
     var ip = cols[5];
     var port = cols[6];
     var server_no = cols[3];
-    var user_no = cols[1];
-    sendMessage(ip,port,server_no,user_no,msg);
-    console.log('Received: ' + data);
+    var user_no = "1";
+    sendMessage(ip,port,server_no,user_no,msg,callback);
     client.destroy(); // kill client after server's response
   });
 
   client.on('close', function() {
-    console.log('Connection closed 1');
+    ///console.log('Connection closed 1');
   });
 
 }
 
-module.export = {
-  "getMessageServer": getMessageServer,
-  "recv": recv
+module.exports = {
+  "getMessageServer": getMessageServer
 };
--- tests/test.js
+++
@@ -1,4 +0,0 @@
-var a = require("../routes/esb/iQA");
-console.log(a.recv);
-console.log(a.getMessageServer("台北分行經理是誰"));
-console.log(a.recv);
